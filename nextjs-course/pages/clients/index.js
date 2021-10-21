b0cf146d77a4d22f7e33d8db41d72117e4882f62
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-21 11:20:45 +0200

    section 3-61

--- nextjs-course/pages/clients/index.js
+++ nextjs-course/pages/clients/index.js
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 function ClientsPage() {
-    return (
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+  return (
     <div>
-        <h1>The Clients Page</h1>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
-    );
+  );
 }
 
 export default ClientsPage;
